bcc42e93341a5defcd3b6f2ceee7d9b62e27bc4e
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-01-12 20:53:45 -0800

    Start brute_force

--- groupmaker/__main__.py
+++ groupmaker/__main__.py
@@ -1,8 +1,10 @@
 import reader
 import sys
+import brute_force
 
 # Get class preferences
 class_prefs = reader.get_class_prefs("")
 
 print(sys.argv)
 print(class_prefs)
+print(brute_force.randomize(class_prefs))
--- groupmaker/brute_force.py
+++ groupmaker/brute_force.py
@@ -1,3 +1,22 @@
+import random as rnd
+
 def randomize(prefs_dict):
     # Give everyone a random choice, but split it up so that there are an equal
     # number of choices
+
+    # Dict containing the choice given to each student
+    results = []
+
+    # To keep track of how many students are assigned each choice
+    choice_counter = [0, 0, 0, 0]
+
+    # Each element corresponds to a different student
+    num_of_students = len(prefs_dict)
+
+    for student_prefs in prefs_dict:
+        given_choice = rnd.randint(1, 4)
+        results.append(given_choice)
+        # Increment the counter
+        choice_counter[given_choice - 1] = choice_counter[given_choice - 1] + 1
+
+    return results
