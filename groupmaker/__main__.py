619391671347e87c771a9c12998007308ba284e6
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2018-01-12 19:36:30 -0800

    Start project

---
+++ groupmaker/__main__.py
@@ -0,0 +1,4 @@
+import reader
+
+# Get class preferences
+class_prefs = reader.get_class_prefs("")
---
+++ groupmaker/reader.py
@@ -0,0 +1,5 @@
+def get_class_prefs(path_to_file):
+    # Function to read text file containing class preferences and return it as a
+    # dict
+
+    return []
